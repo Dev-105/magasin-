ed3fd9b37a4e69769916c273d2e75154691b7f12
Rfifisa <rfifisa@gmail.com>
2026-05-06 21:07:39 +0100

    apis good

--- api/app/Http/Controllers/API/OrderController.php
+++ api/app/Http/Controllers/API/OrderController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Command;
 use App\Models\LigneCommande;
 use App\Models\CartItem;
+use App\Models\Product;
 use App\Models\PromoCode;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
